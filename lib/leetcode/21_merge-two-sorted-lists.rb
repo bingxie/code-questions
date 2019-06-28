90ddc7f4260879488b5e90d8c0e6176f123d577a
Bing Xie <bing708@gmail.com>
2019-06-28 13:52:09 +1000

    Add merge two lists recursion method

--- lib/leetcode/21_merge-two-sorted-lists.rb
+++ lib/leetcode/21_merge-two-sorted-lists.rb
@@ -28,6 +28,20 @@ def merge_two_lists(l1, l2)
   dummy_head.next
 end
 
+def merge_two_lists_recursion(l1, l2)
+  if l1.nil?
+    l2
+  elsif l2.nil?
+    l1
+  elsif l1.val < l2.val
+    l1.next = merge_two_lists_recursion(l1.next, l2)
+    l1
+  else
+    l2.next = merge_two_lists_recursion(l1, l2.next)
+    l2
+  end
+end
+
 node1 = ListNode.new(1)
 node2 = ListNode.new(4)
 node3 = ListNode.new(5)
@@ -45,3 +59,5 @@ node5.next = node6
 node6.next = node7
 
 pp merge_two_lists(l1, l2)
+
+pp merge_two_lists_recursion(l1, l2)
