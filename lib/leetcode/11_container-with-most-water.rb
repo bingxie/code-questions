1b8175143b806576f5d6d4b2efdd4e668b0a1681
Bing Xie <bing708@gmail.com>
2019-12-10 16:18:13 +1100

    More questions

---
+++ lib/leetcode/11_container-with-most-water.rb
@@ -0,0 +1,25 @@
+# @param {Integer[]} height
+# @return {Integer}
+def max_area(height)
+  result = 0
+
+  left = 0
+  right = height.size - 1
+
+  while left < right
+    new_area = (right - left) * [height[left], height[right]].min
+    result = [new_area, result].max
+
+    if height[left] <= height[right]
+      left += 1
+    else
+      right -= 1
+    end
+  end
+
+  result
+end
+
+height = [1,8,6,2,5,4,8,3,7]
+
+p max_area(height)
---
+++ lib/others/manager_employees.rb
@@ -0,0 +1,47 @@
+class Solution
+  def initialize
+    @employees = {}
+    @manager_map = Hash.new {|h, key| h[key] = []}
+  end
+
+  def print_manager_employees(input)
+    parse_input(input)
+
+    @manager_map['0'].each do |top_manager|
+      puts @employees[top_manager].name
+      print_members(top_manager, '-')
+    end
+  end
+
+  private
+
+  def parse_input(input)
+    persons = input.split(', ')
+
+    persons.each do |person|
+      values = person.split(':')
+      employee = Employee.new(*values)
+
+      @employees[employee.id] = employee
+      @manager_map[employee.manager_id].push employee.id
+    end
+  end
+
+  def print_members(manager_id, prefix)
+    @manager_map[manager_id]&.each do |employee_id|
+      puts "#{prefix} #{@employees[employee_id].name}"
+
+      print_members(employee_id, prefix * 2)
+    end
+  end
+end
+
+class Employee < Struct.new(:id, :name, :manager_id)
+end
+
+# 4: [1, 2]
+# 0: [4]
+# 1: [3]
+
+input = '1:Max:4, 4:Ann:0, 2:Jim:4, 3:Tom:1'
+Solution.new.print_manager_employees(input)
